22d47af8638180534e58e1c386c7aaf5767155b0
Furkan Atakan BOZKURT <fab@nibgat.com>
2025-07-30 18:43:44 +0300

    Update options.ts
    scrollTreshold type added.

--- src/options.ts
+++ src/options.ts
@@ -42,7 +42,7 @@ export interface IConfigProps {
 }
 
 export interface IProps<ListItem = any> {
-  isMahmutTabClose?: boolean;
+  scrollTreshold?: number;
   /**
    * A reference to the view (ScrollView, FlatList, SectionList) that provides the scroll behavior, where you will be able to access their owns methods.
    */
